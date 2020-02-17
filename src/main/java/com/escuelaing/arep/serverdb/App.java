310845a270a6498e02a3537f5a1189edacb1e15c
NicoCardenas <nicolas.cardenas@mail.escuelaing.edu.co>
2020-02-16 22:35:40 -0500

    port

--- src/main/java/com/escuelaing/arep/serverdb/App.java
+++ src/main/java/com/escuelaing/arep/serverdb/App.java
@@ -18,6 +18,7 @@ public class App
             Class.forName("org.postgresql.Driver");
             String host = "ec2-35-168-54-239.compute-1.amazonaws.com";
             String db = "d8g67as15jh72k";
+            String port = "5432";
             String user = "wwycfalrdvlodn";
             String passwd = "a6d4b6436e88f6fee723d67818cc6c4a83576b2bfe73fe66f7793e0f70ddef4d";
             Connection con = DriverManager.getConnection( "jdbc:postgresql://"+ host + ":" + port + "/" + db, user, passwd);
